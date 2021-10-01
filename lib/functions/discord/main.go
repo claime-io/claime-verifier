acec06fcc31085e9deb45fc6982b0c9d0388d68d
atatur9 <momodarou2015@gmail.com>
2021-10-02 02:05:06 +0900

    discord auth

--- lib/functions/discord/main.go
+++ lib/functions/discord/main.go
@@ -1,27 +1,92 @@
 package main
 
 import (
+	"bytes"
 	"claime-verifier/lib/functions/lib"
+	"claime-verifier/lib/functions/lib/common/log"
 	slackclient "claime-verifier/lib/functions/lib/infrastructure/slack"
 	"context"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"claime-verifier/lib/functions/lib/subscribe"
 	repository "claime-verifier/lib/functions/lib/subscribe/persistence"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/bwmarrin/discordgo"
 )
 
 const (
 	requiredArgs = 3
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, request map[string]interface{}) (events.APIGatewayProxyResponse, error) {
+	token := request["signature"].(string)
+	timestamp := request["timestamp"].(string)
+
+	req, _ := json.Marshal(request["jsonBody"])
+	webhook := discordgo.Webhook{}
+	json.Unmarshal(req, &webhook)
+	httpreq, err := http.NewRequest("", "", bytes.NewReader(req))
+	if err != nil {
+		log.Error("", err)
+	}
+	key, _ := hex.DecodeString("API_KEY")
+	if err != nil {
+		log.Error("", err)
+	}
+	httpreq.Header.Add("X-Signature-Ed25519", token)
+	httpreq.Header.Add("X-Signature-Timestamp", timestamp)
+	result := discordgo.VerifyInteraction(httpreq, key)
+	fmt.Println(result)
+	if !result {
+		return events.APIGatewayProxyResponse{
+			StatusCode:      401,
+			Body:            `[UNAUTHORIZED] invalid request signature`,
+			Headers:         map[string]string{},
+			IsBase64Encoded: false,
+		}, nil
+	}
+	fmt.Println("type")
+	fmt.Println(webhook.Type)
+
+	if webhook.Type == discordgo.WebhookTypeChannelFollower {
+		resp := struct {
+			Type int `json:"type"`
+		}{
+			Type: 1,
+		}
+		rp, _ := json.Marshal(resp)
+		return events.APIGatewayProxyResponse{
+			StatusCode:      200,
+			Body:            string(rp),
+			Headers:         map[string]string{},
+			IsBase64Encoded: false,
+		}, nil
+	}
+	if webhook.Type == discordgo.WebhookTypeIncoming {
+		resp := struct {
+			Type int `json:"type"`
+		}{
+			Type: 5,
+		}
+		rp, _ := json.Marshal(resp)
+		return events.APIGatewayProxyResponse{
+			StatusCode:      200,
+			Body:            string(rp),
+			Headers:         map[string]string{},
+			IsBase64Encoded: false,
+		}, nil
+	}
 
 	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Headers:    lib.Headers(request),
-		Body:       `{"type": "1"}`,
+		StatusCode:      401,
+		Body:            `[UNAUTHORIZED] invalid request signature`,
+		Headers:         map[string]string{},
+		IsBase64Encoded: false,
 	}, nil
 }
 
