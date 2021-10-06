08bbeb8d9024e258ca76997a14833a5716445b4a
atatur9 <momodarou2015@gmail.com>
2021-10-06 19:16:20 +0900

    update tests

--- test/snapshot-plugin.ts
+++ test/snapshot-plugin.ts
@@ -2,7 +2,7 @@ module.exports = {
   test: (val: any) => typeof val === 'string',
   serialize: (val: any) => {
     return `"${val.replace(
-      /AssetParameters([A-Fa-f0-9]{64})(\w+)|(\w+) (\w+) for asset\s?(version)?\s?"([A-Fa-f0-9]{64})"|RestApiDeployment([A-Za-z0-9]{1,})/,
+      /AssetParameters([A-Fa-f0-9]{64})(\w+)|(\w+) (\w+) for asset\s?(version)?\s?"([A-Fa-f0-9]{64})"|RestApiDeployment([A-Za-z0-9]{1,})|assets:([A-Za-z0-9]{1,})/,
       '[HASH REMOVED]',
     )}"`
   },
