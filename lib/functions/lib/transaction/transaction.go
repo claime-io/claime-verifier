0c5347b7217f92fd4c474d3f4183ccdd7676dab2
Shoya-Y <touka.nt.gh@gmail.com>
2021-10-08 04:00:13 +0900

    enable to recover claim from tx

--- lib/functions/lib/transaction/transaction.go
+++ lib/functions/lib/transaction/transaction.go
@@ -2,9 +2,12 @@ package transaction
 
 import (
 	"claime-verifier/lib/functions/lib/common/log"
+	"claime-verifier/lib/functions/lib/contracts"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 func RecoverAddress(rawTx string, signature string) string {
@@ -32,4 +35,32 @@ func RecoverAddress(rawTx string, signature string) string {
 	}
 	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
 	return recoveredAddr.Hex()
+}
+
+func RecoverClaim(rawTx string) (contracts.IClaimRegistryClaim, error) {
+	var result []interface{}
+	arr, err := hexutil.Decode(rawTx)
+	if err != nil {
+		return contracts.IClaimRegistryClaim{}, err
+	}
+	err = rlp.DecodeBytes(arr[1:], &result)
+	if err != nil {
+		return contracts.IClaimRegistryClaim{}, err
+	}
+	data := result[7].([]byte)
+	val, err := abi.Arguments{
+		abi.Argument{Type: abi.Type{T: abi.StringTy}},
+		abi.Argument{Type: abi.Type{T: abi.StringTy}},
+		abi.Argument{Type: abi.Type{T: abi.StringTy}},
+		abi.Argument{Type: abi.Type{T: abi.StringTy}},
+	}.UnpackValues(data[4:])
+	if err != nil {
+		return contracts.IClaimRegistryClaim{}, err
+	}
+	return contracts.IClaimRegistryClaim{
+		PropertyType: val[0].(string),
+		PropertyId:   val[1].(string),
+		Evidence:     val[2].(string),
+		Method:       val[3].(string),
+	}, nil
 }
