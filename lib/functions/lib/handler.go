5517f18ccdec51ad3dbfa26ae73d8a9d7ced9bc5
Shoya-Y <touka.nt.gh@gmail.com>
2021-10-10 11:39:08 +0900

    remove unused

--- lib/functions/lib/handler.go
+++ lib/functions/lib/handler.go
@@ -1,13 +1,7 @@
 package lib
 
 import (
-	"claime-verifier/lib/functions/lib/common/log"
-	slackclient "claime-verifier/lib/functions/lib/infrastructure/slack"
-	"claime-verifier/lib/functions/lib/infrastructure/ssm"
-	"context"
 	"os"
-
-	"github.com/aws/aws-lambda-go/events"
 )
 
 // Headers with headers
@@ -18,27 +12,4 @@ func Headers() map[string]string {
 		"Access-Control-Allow-Credentials": "true",
 		"Access-Control-Allow-Origin":      os.Getenv("AllowedOrigin"),
 	}
-}
-
-// NewParameterStore new parameter store
-func NewParameterStore(ctx context.Context) (ssm.Client, error) {
-	return ssm.New(ctx), nil
-}
-
-// Verify verify slack message
-func Verify(request events.APIGatewayProxyRequest, cli slackclient.Client) error {
-	return cli.Verify(request)
-}
-
-// Slackcli new slack client
-func Slackcli(parameterstore ssm.Client) (slackclient.Client, error) {
-	token, err := parameterstore.SlackToken()
-	if err != nil {
-		log.Error("retrive token failed", err)
-	}
-	signingsecret, err := parameterstore.SlackSigningSecret()
-	if err != nil {
-		log.Error("retrive sign secret failed", err)
-	}
-	return slackclient.New(token, signingsecret), nil
 }
