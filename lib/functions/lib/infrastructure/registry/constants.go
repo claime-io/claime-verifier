2a061f45fd7adbe26008ec7cf33ff358e4472982
Shoya-Y <touka.nt.gh@gmail.com>
2021-12-06 18:38:45 +0900

    add contract address in polygon

--- lib/functions/lib/infrastructure/registry/constants.go
+++ lib/functions/lib/infrastructure/registry/constants.go
@@ -10,6 +10,7 @@ import (
 const (
 	claimeRegistryAddressRinkeby = "0xD721AF405fb939fFeBF7B44b294D9D02A232b359"
 	claimeRegistryAddressMumbai  = "0x9b67374857503dA14209844598B0e65fA022Ac1B"
+	claimeRegistryAddressPolygon = "0x7Cac4b4a233849b301b4b651666C3f8cCcb834e2"
 	claimeRegistryAddressMainnet = "0xb52E96533528eD66AbFC3a9680A998a4eBe0E35a"
 )
 
@@ -17,6 +18,9 @@ func registryAddress(network string) (string, error) {
 	env := os.Getenv("EnvironmentId")
 	if evmnetwork.Mainnet.Equals(network) {
 		return claimeRegistryAddressMainnet, nil
+	}
+	if evmnetwork.Polygon.Equals(network) {
+		return claimeRegistryAddressPolygon, nil
 	}
 	if env != "prod" {
 		if evmnetwork.Rinkeby.Equals(network) {
